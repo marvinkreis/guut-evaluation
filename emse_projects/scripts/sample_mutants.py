ec2609f1fb058eca7b591a656b3156d0ab338402
Marvin Kreis <MarvinKreis@web.de>
2024-09-30 15:48:52 +0200

    Sample mutants again

--- emse_projects/scripts/sample_mutants.py
+++ emse_projects/scripts/sample_mutants.py
@@ -6,6 +6,23 @@ from pathlib import Path
 NUM_MUTANTS = 1000
 
 
+def delete_mutants(cur: sqlite3.Cursor, mutants):
+    cur.executemany(
+        "delete from mutation_specs where job_id = ?;",
+        [m[3:] for m in mutants],
+    )
+
+    cur.executemany(
+        "delete from work_items where job_id = ?;",
+        [m[3:] for m in mutants],
+    )
+
+    cur.executemany(
+        "delete from work_results where job_id = ?;",
+        [m[3:] for m in mutants],
+    )
+
+
 def sample_mutants(session_file: Path):
     con = sqlite3.connect(session_file)
     cur = con.cursor()
@@ -13,32 +30,23 @@ def sample_mutants(session_file: Path):
     cur.execute(
         "select module_path, operator_name, occurrence, job_id from mutation_specs;"
     )
-    mutants = cur.fetchall()
+    mutants = set(cur.fetchall())
     print(f"found {len(mutants)} mutants")
 
+    test_mutants = [m for m in mutants if "test" in m[0]]
+    if test_mutants:
+        print(f"found {len(test_mutants)} mutants for test code")
+        delete_mutants(cur, test_mutants)
+        mutants.difference_update(test_mutants)
+
     if len(mutants) > NUM_MUTANTS:
-        sampled_mutants = random.sample(mutants, NUM_MUTANTS)
-        mutant_difference = set(mutants).difference(sampled_mutants)
+        sampled_mutants = random.sample(list(mutants), NUM_MUTANTS)
+        delete_mutants(cur, mutants.difference(sampled_mutants))
 
-        cur.executemany(
-            "delete from mutation_specs where job_id = ?;",
-            [m[3:] for m in mutant_difference],
-        )
-
-        cur.executemany(
-            "delete from work_items where job_id = ?;",
-            [m[3:] for m in mutant_difference],
-        )
-
-        cur.executemany(
-            "delete from work_results where job_id = ?;",
-            [m[3:] for m in mutant_difference],
-        )
-
-        con.commit()
-        con.execute("vacuum;")
-        con.commit()
-        con.close()
+    con.commit()
+    con.execute("vacuum;")
+    con.commit()
+    con.close()
 
 
 if __name__ == "__main__":
