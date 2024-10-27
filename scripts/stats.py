9f81fb774ae3db2385bebfb549e37e97192d22ff
Marvin Kreis <MarvinKreis@web.de>
2024-10-27 22:11:50 +0100

    Update stats script

--- scripts/stats.py
+++ scripts/stats.py
@@ -1,50 +1,139 @@
 # %% Imports, constants and helpers
 
-from functools import partial, reduce
 import json
-from pathlib import Path
+import os
 import sqlite3
-from typing import List, Literal, Union
+from functools import partial, reduce
+from multiprocessing import Pool
+from pathlib import Path
+from typing import Any, Dict, Iterable, List, Literal, Tuple
+from matplotlib import colormaps as cm
 
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
-import os
 
 pd.set_option("display.width", None)
 pd.set_option("display.max_colwidth", None)
 
-RESULTS_DIR = Path(os.getcwd()) / "loops"
-_evaluation_dir = next(iter([path for path in Path(os.getcwd()).parents if path.name == "guut-evaluation"]))
-SESSION_FILE = Path(_evaluation_dir) / "emse_projects/mutants_sampled/python-string-utils.sqlite"
+JsonObj = Dict[str, Any]
+
+# %% Find result directory and mutants
+
+if "get_ipython" in locals():
+    print("Running as ipython kernel")
+    RESULTS_DIR = Path(os.getcwd()).parent / "guut_emse_results"
+    MUTANTS_DIR = Path(os.getcwd()).parent / "emse_projects_data" / "mutants_sampled"
+else:
+    print("Running as script")
+    RESULTS_DIR = Path(__file__).parent / "guut_emse_results"
+    MUTANTS_DIR = Path(__file__).parent / "emse_projects_data" / "mutants_sampled"
 
 
 # %% Load json result files
 
-results_json = []
-for root, dirs, files in RESULTS_DIR.walk():
-    result_paths = (root / name for name in files if name == "result.json")
-    for path in result_paths:
-        result = json.loads(path.read_text())
-        results_json.append(result)
+if "data" in locals():
+    raise Exception("data is already in memory. Refusing to read the files again.")
 
-# Sort!
-results_json.sort(key=lambda result: result["timestamp"])
 
-data = pd.json_normalize(results_json)
+def prepare_loop_result(loop_result: JsonObj):
+    """Delete some unnecessary data to save memory."""
 
-# %% Load mutants
+    # Prepare tests
+    for test in loop_result["tests"]:
+        # Clean execution results
+        if (result := test["result"]) is not None:
+            for exec_result in [result["correct"], result["mutant"]]:
+                if coverage := exec_result["coverage"]:
+                    del coverage["raw"]
 
-con = sqlite3.connect(SESSION_FILE)
-cur = con.cursor()
-cur.execute("select module_path, operator_name, occurrence, start_pos_row, end_pos_row from mutation_specs;")
-mutants = cur.fetchall()
+    # Prepare experiments
+    for experiment in loop_result["experiments"]:
+        # Clean execution results
+        if (result := experiment["result"]) is not None:
+            for exec_result in [
+                result["test_correct"],
+                result["test_mutant"],
+                result["debug_correct"],
+                result["debug_mutant"],
+            ]:
+                if exec_result is None:
+                    continue
+                if coverage := exec_result["coverage"]:
+                    del coverage["raw"]
 
-# %% Load mutant line info into the table
+    # Prepare messages
+    for msg in loop_result["conversation"]:
+        del msg["content"]
 
-mutant_lines = {(m[0], m[1], m[2]): (m[3], m[4]) for m in mutants}
+
+def read_result_full(path: Path) -> JsonObj:
+    with path.open("r") as f:
+        result = json.load(f)
+    prepare_loop_result(result)
+    return result
+
+
+print("Reading tons of json files. This may take a while...")
+with Pool(8) as pool:
+    results_json = pool.imap_unordered(read_result_full, RESULTS_DIR.glob("*/loops/*/result.json"), chunksize=100)
+    data = pd.json_normalize(results_json)
+    data = data.sort_values(by=["long_id", "problem.target_path", "problem.mutant_op", "problem.occurrence"])
+    del results_json
+print("done")
+
+# %% Print memory usage
+
+with open("/proc/self/status") as f:
+    memusage = f.read().split("VmRSS:")[1].split("\n")[0][:-3]
+    print(f"Memory used: {int(memusage.strip()) / (1024**2):.3f} GB")
+
+# %% Add project name to data
+
+package_to_project = {
+    "apimd": "apimd",
+    "black": "black",
+    "blackd": "black",
+    "black_primer": "black",
+    "blib2to3": "black",
+    "_black_version": "black",
+    "codetiming": "codetiming",
+    "dataclasses_json": "dataclasses-json",
+    "docstring_parser": "docstring_parser",
+    "flake8": "flake8",
+    "flutes": "flutes",
+    "flutils": "flutils",
+    "httpie": "httpie",
+    "isort": "isort",
+    "mimesis": "mimesis",
+    "pdir": "pdir2",
+    "py_backwards": "py-backwards",
+    "pymonet": "pyMonet",
+    "pypara": "pypara",
+    "semantic_release": "python-semantic-release",
+    "string_utils": "python-string-utils",
+    "pytutils": "pytutils",
+    "sanic": "sanic",
+    "sty": "sty",
+    "thonny": "thonny",
+    "typesystem": "typesystem",
+}
+data["project"] = data["problem.module_path"].map(lambda path: package_to_project[path.rsplit("/")[-1]])
+
+# %% Add lines of mutants
+
+
+def read_mutant_lines(mutants_file: Path) -> Dict[Tuple[str, str, str], Tuple[int, int]]:
+    con = sqlite3.connect(mutants_file)
+    cur = con.execute("select module_path, operator_name, occurrence, start_pos_row, end_pos_row from mutation_specs;")
+    mutants = cur.fetchall()
+    con.close()
+    return {(m[0], m[1], m[2]): (m[3], m[4]) for m in mutants}
+
+
+mutant_lines = {project: read_mutant_lines(MUTANTS_DIR / f"{project}.sqlite") for project in data["project"].unique()}
 data["mutant_lines"] = data.apply(
-    lambda row: mutant_lines[
+    lambda row: mutant_lines[row["project"]][
         (
             row["problem.target_path"],
             row["problem.mutant_op"],
@@ -55,11 +144,13 @@ data["mutant_lines"] = data.apply(
 )
 data["mutant_lines.start"] = data["mutant_lines"].map(lambda t: t[0])
 data["mutant_lines.end"] = data["mutant_lines"].map(lambda t: t[1])
+del mutant_lines
+
 
 # %% Helper functions
 
 
-def get_result(exp_or_test: Union["Experiment", "Test"], mutant: bool = False) -> "ExecutionResult":
+def get_result(exp_or_test: JsonObj, mutant: bool = False) -> JsonObj | None:
     if exp_or_test is None:
         return None
 
@@ -73,18 +164,32 @@ def get_result(exp_or_test: Union["Experiment", "Test"], mutant: bool = False) -
         return result.get("test_mutant") or result.get("mutant")
 
 
-def find_killing_test(tests: List["Test"]) -> "Test":
+def find_killing_test(tests: List[JsonObj]) -> JsonObj | None:
     killing_tests = [test for test in tests if test["kills_mutant"]]
     return killing_tests[0] if killing_tests else None
 
 
-def subtract_coverage(x: List[int], y: List[int]) -> List[int]:
-    acc = set(x)
-    acc.intersection_update(y)
-    return list(acc)
+# %% Strings to colors
 
 
-# %% Check whether each run covers the mutant
+cmap = cm.get_cmap("tab10")
+
+
+def string_colors(strings: Iterable[str], num_colors=10):
+    known_strings = {}
+    current_color = 0
+    colors = []
+    for string in strings:
+        if color := known_strings.get(string):
+            colors.append(color)
+        else:
+            known_strings[string] = current_color
+            colors.append(current_color)
+            current_color = (current_color + 1) % num_colors
+    return colors
+
+
+# %% Check whether each run covers the mutant. This is not very accurate.
 
 
 def covers_mutant(row, experiments_or_tests: Literal["experiments", "tests"]):
@@ -101,6 +206,7 @@ data["experiment_covers_mutant"] = data.apply(partial(covers_mutant, experiments
 data["test_covers_mutant"] = data.apply(partial(covers_mutant, experiments_or_tests="tests"), axis=1)
 data["mutant_covered"] = data["experiment_covers_mutant"] | data["test_covers_mutant"]
 
+
 # %% Compute coverage from killing tests
 
 
@@ -114,11 +220,61 @@ def get_coverage_from_test(test):
 
 data["coverage.covered_lines"] = data["tests"].map(lambda tests: get_coverage_from_test(find_killing_test(tests))[0])
 data["coverage.missing_lines"] = data["tests"].map(lambda tests: get_coverage_from_test(find_killing_test(tests))[1])
+data["coverage.covered_line_ids"] = data.apply(
+    lambda row: [f'{row["problem.target_path"]}::{line}' for line in row["coverage.covered_lines"]], axis=1
+)
+data["coverage.missing_line_ids"] = data.apply(
+    lambda row: [f'{row["problem.target_path"]}::{line}' for line in row["coverage.missing_lines"]], axis=1
+)
+
+
+# %% Count number of coverable lines and number of mutants per module
+
+all_lines = {}
+all_line_ids = {}
+num_mutants = {}
+
+for project in data["project"].unique():
+    for target in data[data["project"] == project]["problem.target_path"].unique():
+        covered = data[
+            (data["project"] == project) & (data["problem.target_path"] == target) & (data["coverage.covered_lines"])
+        ]
+        if len(covered):
+            all_lines[(project, target)] = (
+                covered.iloc[0]["coverage.covered_lines"] + covered.iloc[0]["coverage.missing_lines"]
+            )
+            all_line_ids[(project, target)] = (
+                covered.iloc[0]["coverage.covered_line_ids"] + covered.iloc[0]["coverage.missing_line_ids"]
+            )
+
+        num_mutants[(project, target)] = len(
+            data[
+                (data["project"] == project)
+                & (data["problem.target_path"] == target)
+                & (data["settings.preset_name"] == "debugging_one_shot")
+            ]
+        )
+
+data["num_mutants_in_module"] = data.apply(
+    lambda row: num_mutants[(row["project"], row["problem.target_path"])], axis=1
+)
+data["coverage.all_lines"] = data.apply(
+    lambda row: all_lines.get((row["project"], row["problem.target_path"]), []), axis=1
+)
+data["coverage.num_lines"] = data["coverage.all_lines"].map(len)
+data["coverage.all_line_ids"] = data.apply(
+    lambda row: all_line_ids.get((row["project"], row["problem.target_path"]), []), axis=1
+)
+
+del num_mutants
+del all_lines
+del all_line_ids
+
 
 # %% Plot Coverage
 
 
-def add_coverage(x: List[int], y: List[int]) -> List[int]:
+def add_coverage(x: List[str], y: List[str]) -> List[str]:
     acc = set(x)
     acc.update(y)
     return list(acc)
@@ -126,25 +282,38 @@ def add_coverage(x: List[int], y: List[int]) -> List[int]:
 
 add_coverage_np = np.frompyfunc(add_coverage, 2, 1)
 
-for target_path in np.unique(data[["problem.target_path"]]):
-    target_data = data[data["problem.target_path"] == target_path]
 
-    all_lines = set(reduce(add_coverage, target_data["coverage.covered_lines"], []))
-    all_lines.update(reduce(add_coverage, target_data["coverage.missing_lines"], []))
-
-    if not all_lines:
-        continue
-
-    acc_coverage = add_coverage_np.accumulate(target_data["coverage.covered_lines"])
-    acc_coverage_percent = acc_coverage.map(lambda c: len(c) / len(all_lines))
-
-    x = np.arange(len(acc_coverage))
+for project in np.unique(data[["project"]]):
     fig, ax = plt.subplots(layout="constrained", figsize=(8, 4))
-    ax.set_ylim(0, 1)
-    ax.plot(x, acc_coverage_percent, 0.6, label="accumulated coverage")
-    ax.tick_params(axis="x", rotation=90)
-    ax.legend(loc="upper left")
-    ax.text(0, 0.8, target_path)
+    for preset in [
+        "baseline_without_iterations",
+        "baseline_with_iterations",
+        "debugging_zero_shot",
+        "debugging_one_shot",
+    ]:
+        target_data = data[(data["project"] == project) & (data["settings.preset_name"] == preset)]
+        target_data = target_data.sort_values(by=["num_mutants_in_module", "problem.module_path"], ascending=False)
+
+        acc_coverage = add_coverage_np.accumulate(target_data["coverage.covered_line_ids"])
+        all_lines = reduce(add_coverage, target_data["coverage.all_line_ids"], [])
+        acc_coverage_percent = acc_coverage.map(lambda c: len(c) / max(len(all_lines), 1))
+
+        x = np.arange(len(acc_coverage_percent))
+        y = acc_coverage_percent
+
+        ax.plot(x, acc_coverage_percent, 0.6, label=f"{preset}")
+
+    current_target = ""
+    lines = []
+    for index, (_, row) in enumerate(target_data.iterrows()):
+        new_target = row["problem.target_path"]
+        if new_target != current_target:
+            current_target = new_target
+            lines.append(index)
+    ax.vlines(x=lines, ymin=0.0, ymax=1.0, colors="lightgrey", ls="--", lw=1, label="modules")
+
+    fig.text(0.00, 1.02, project)
+    ax.legend()
     plt.show()
 
 # %% Compute Token Usage
@@ -504,14 +673,42 @@ data[data["mutant_killed"]]["usage.cost"].mean()
 
 # %% Compute Token Usage of first 10 messages
 
-data["usage.prompt_tokens.f10"] = data["conversation"].map(
+data["num_messages.l10"] = data["conversation"].map(
+    lambda c: len([msg for msg in c if msg["role"] == "assistant"][10:])
+)
+data["usage.prompt_tokens.l10"] = data["conversation"].map(
     lambda c: sum([msg["usage"]["prompt_tokens"] for msg in c if msg["role"] == "assistant"][10:])
 )
-data["usage.completion_tokens.f10"] = data["conversation"].map(
+data["usage.completion_tokens.l10"] = data["conversation"].map(
     lambda c: sum([msg["usage"]["completion_tokens"] for msg in c if msg["role"] == "assistant"][10:])
 )
-data["usage.cached_tokens.f10"] = data["conversation"].map(
+data["usage.cached_tokens.l10"] = data["conversation"].map(
     lambda c: sum([msg["usage"].get("cached_tokens", 0) for msg in c if msg["role"] == "assistant"][10:])
+)
+data["usage.total_tokens.l10"] = data["usage.prompt_tokens.l10"] + data["usage.completion_tokens.l10"]
+
+# Add cost in $ for gpt-4o-mini
+# prompt: $0.150 / 1M input tokens
+# cached prompt: $0.075 / 1M input tokens
+# completion: $0.600 / 1M input tokens
+data["usage.cost.l10"] = (
+    ((data["usage.prompt_tokens.l10"] - data["usage.cached_tokens.l10"]) * 0.150)
+    + (data["usage.cached_tokens.l10"] * 0.075)
+    + (data["usage.completion_tokens.l10"] * 0.600)
+) / 1_000_000
+# %% Compute Token Usage of last 10 messages
+
+data["num_messages.f10"] = data["conversation"].map(
+    lambda c: len([msg for msg in c if msg["role"] == "assistant"][:10])
+)
+data["usage.prompt_tokens.f10"] = data["conversation"].map(
+    lambda c: sum([msg["usage"]["prompt_tokens"] for msg in c if msg["role"] == "assistant"][:10])
+)
+data["usage.completion_tokens.f10"] = data["conversation"].map(
+    lambda c: sum([msg["usage"]["completion_tokens"] for msg in c if msg["role"] == "assistant"][:10])
+)
+data["usage.cached_tokens.f10"] = data["conversation"].map(
+    lambda c: sum([msg["usage"].get("cached_tokens", 0) for msg in c if msg["role"] == "assistant"][:10])
 )
 data["usage.total_tokens.f10"] = data["usage.prompt_tokens.f10"] + data["usage.completion_tokens.f10"]
 
@@ -525,28 +722,13 @@ data["usage.cost.f10"] = (
     + (data["usage.completion_tokens.f10"] * 0.600)
 ) / 1_000_000
 
-# %% Compute Token Usage of last 10 messages
+# %%
 
-data["usage.prompt_tokens.l10"] = data["conversation"].map(
-    lambda c: sum([msg["usage"]["prompt_tokens"] for msg in c if msg["role"] == "assistant"][:10])
-)
-data["usage.completion_tokens.l10"] = data["conversation"].map(
-    lambda c: sum([msg["usage"]["completion_tokens"] for msg in c if msg["role"] == "assistant"][:10])
-)
-data["usage.cached_tokens.l10"] = data["conversation"].map(
-    lambda c: sum([msg["usage"].get("cached_tokens", 0) for msg in c if msg["role"] == "assistant"][:10])
-)
-data["usage.total_tokens.l10"] = data["usage.prompt_tokens.l10"] + data["usage.completion_tokens.l10"]
+data["num_messages.f10"].sum()
 
-# Add cost in $ for gpt-4o-mini
-# prompt: $0.150 / 1M input tokens
-# cached prompt: $0.075 / 1M input tokens
-# completion: $0.600 / 1M input tokens
-data["usage.cost.l10"] = (
-    ((data["usage.prompt_tokens.l10"] - data["usage.cached_tokens.l10"]) * 0.150)
-    + (data["usage.cached_tokens.l10"] * 0.075)
-    + (data["usage.completion_tokens.l10"] * 0.600)
-) / 1_000_000
+# %%
+
+data["num_messages.l10"].sum()
 
 # %%
 
