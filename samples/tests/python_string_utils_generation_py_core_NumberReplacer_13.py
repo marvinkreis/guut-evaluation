cc68719149eb1f2e093faa5004d84fcfcacf9377
Marvin Kreis <MarvinKreis@web.de>
2024-12-10 21:01:17 +0100

    Change same samples again

--- samples/tests/python_string_utils_generation_py_core_NumberReplacer_13.py
+++ samples/tests/python_string_utils_generation_py_core_NumberReplacer_13.py
@@ -7,12 +7,4 @@ class MockInt(int):
         return super().__gt__(other)
 
 def test():
-    orig_isinstance = __builtins__["isinstance"]
-
-    def mock_isinstance(obj, cls):
-        if orig_isinstance(obj, cls):
-            return True
-        return orig_isinstance(obj, MockInt)
-
-    __builtins__["isinstance"] = mock_isinstance
     roman_range(2, 1, MockInt(1))
--- samples/tests/python_string_utils_generation_py_core_ReplaceComparisonOperator_Gt_Eq_2.py
+++ samples/tests/python_string_utils_generation_py_core_ReplaceComparisonOperator_Gt_Eq_2.py
@@ -8,14 +8,6 @@ class MockInt(int):
         return super().__eq__(other)
 
 def test():
-    orig_isinstance = __builtins__["isinstance"]
-
-    def mock_isinstance(obj, cls):
-        if orig_isinstance(obj, cls):
-            return True
-        return orig_isinstance(obj, MockInt)
-
-    __builtins__["isinstance"] = mock_isinstance
     try:
         roman_range(MockInt(1), 123, 1)
     except OverflowError:
